dcecc3dbeb5e32c10bb3384e93bd62a8e6773a1d
YELANDAOKONG <yelandaokong@yldk.xyz>
2023-10-28 09:37:04 +0800

    Add TCP

--- ACPLibrary/Core/AcpCoreService.cs
+++ ACPLibrary/Core/AcpCoreService.cs
@@ -5,6 +5,8 @@ namespace ACPLibrary.Core;
 
 public class AcpCoreService
 {
+    private Socket NetworkTcpSocket;
+    private Socket NetworkTcpSocketV6;
     private Socket NetworkUdpSocket;
     private Socket NetworkUdpSocketV6;
     private Socket LocalUdpSocket;
@@ -15,6 +17,14 @@ public class AcpCoreService
 
     public AcpCoreService()
     {
+        this.NetworkTcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        this.NetworkTcpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+        // this.NetworkTcpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+        
+        this.NetworkTcpSocketV6 = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+        this.NetworkTcpSocketV6.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+        // this.NetworkTcpSocketV6.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+        
         this.NetworkUdpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         this.NetworkUdpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
         this.NetworkUdpSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
@@ -30,6 +40,9 @@ public class AcpCoreService
         this.LocalUdpSocketV6 = new Socket(AddressFamily.InterNetworkV6, SocketType.Dgram, ProtocolType.Udp);
         this.LocalUdpSocketV6.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
         this.LocalUdpSocketV6.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, true);
+        
+        this.NetworkTcpSocket.Bind(new IPEndPoint(IPAddress.Any, this.NetworkUdpPort));
+        this.NetworkTcpSocketV6.Bind(new IPEndPoint(IPAddress.IPv6Any, this.NetworkUdpPort));
         
         this.NetworkUdpSocket.Bind(new IPEndPoint(IPAddress.Any, this.NetworkUdpPort));
         this.NetworkUdpSocketV6.Bind(new IPEndPoint(IPAddress.IPv6Any, this.NetworkUdpPort));
